cf6a7fe53f00d86929ddfd621ebe7139775051ce
Lailson Sobral <lailsonlm@hotmail.com>
2022-06-23 10:31:30 -0300

    create routes and call lessons via GraphCMS API

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -4,3 +4,26 @@ export const client = new ApolloClient({
   uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o8bvso175p01xm1ikr6v56/master',
   cache: new InMemoryCache()
 })
+
+
+// Remover o cache
+// import { ApolloClient, DefaultOptions, InMemoryCache } from "@apollo/client";
+
+// const defaultOptions: DefaultOptions = {
+
+
+//   watchQuery: {
+//     fetchPolicy: "no-cache",
+//     errorPolicy: "ignore",
+//   },
+//   query: {
+//     fetchPolicy: "no-cache",
+//     errorPolicy: "all",
+//   },
+// };
+
+// export const client = new ApolloClient({
+//   uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o8bvso175p01xm1ikr6v56/master',
+//   cache: new InMemoryCache(),
+//   defaultOptions: defaultOptions,
+// });
